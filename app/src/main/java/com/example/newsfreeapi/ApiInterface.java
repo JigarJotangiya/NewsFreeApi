f464e1dacb228c4b3d0cd6255865dd66d52dfbc4
JigarJotangiya <jigarjotangiya1999@gmail.com>
2022-04-28 11:47:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsfreeapi/ApiInterface.java
@@ -0,0 +1,20 @@
+package com.example.newsfreeapi;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Query;
+
+interface Apiinterface {
+
+
+    @GET("v1/search")
+    Call<Modeldatum> getData(@Query("q")String q,
+                             @Query("lang")String lang,
+                             @Header("X-RapidAPI-Host")String Host,
+                             @Header("X-RapidAPI-Key")String Key);
+
+};
